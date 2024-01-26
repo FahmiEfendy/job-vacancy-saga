a986b90f8428bd09610ae4ec3018e1d81b98b093
iqbalprasettya <muhamadiqbalprasetya89@gmail.com>
2024-01-26 09:02:20 +0700

    detail page

--- src/pages/JobDetail/index.jsx
+++ src/pages/JobDetail/index.jsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom';
 
 import classes from './style.module.scss';
 
+import WorkIcon from '@mui/icons-material/Work';
+
 // data = {
 //   id: '',
 //   companyName: '',
@@ -14,9 +16,40 @@ import classes from './style.module.scss';
 // };
 
 const JobDetail = () => {
-  const { jobId } = useParams();
+  // const { jobId } = useParams();
 
-  return <>This is Job Detail Page for job with if of {jobId}!</>;
+  return (
+    <>
+      <section className={classes['job-detail']}>
+        <div className={classes['container']}>
+          <div className={classes['job-item']}>
+            <div className={classes['job-head']}>
+              <h1>Staff Admin</h1>
+              <p>
+                PT Multi Hidrachrome Industri - <span>Jakarta</span>
+              </p>
+              <small><WorkIcon className={classes["icon"]} />Magang</small>
+            </div>
+            <div className={classes["job-body"]}>
+            <span>Tanggung Jawab</span>
+              <ul>
+                <li>Membuat Laporan Pembukuan</li>
+                <li>Menginput Data</li>
+              </ul>
+              <span>Persyaratan minimum</span>
+              <ul>
+                <li>Usia Maksimal 30 Tahun</li>
+                <li>Terbiasa Dengan Ms. Office</li>
+                <li>Berpenampilan Rapih</li>
+                <li>Berpengalaman Dibidang Accountan</li>
+                <li>Diutamakan Berdomisili Di Jakarta Barat</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default JobDetail;
